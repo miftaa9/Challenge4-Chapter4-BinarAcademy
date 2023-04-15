08e335000052c311da0cc64144872b02b469db90
miftaaaaaa <miftakhul.20015@mhs.unesa.ac.id>
2023-04-16 02:21:24 +0700

    bcr

---
+++ config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  development: {
+    username: "postgres",
+    password: "miftakhul9",
+    database: "BinarAcademy_Challenge_4",
+    host: "localhost",
+    dialect: "postgres"
+  }
+}
